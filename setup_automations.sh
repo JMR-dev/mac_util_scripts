16767009c135b7588e8a607b5b9d0d92a4ac61ba
Jason Ross <jaross@express.com>
2026-06-09 15:55:29 -0500

    updated boostrap script

--- setup_automations.sh
+++ setup_automations.sh
@@ -5,9 +5,10 @@ fi
 cd "$HOME/Library/LaunchAgents" || exit 1
 curl -LO https://github.com/JMR-dev/mac_util_scripts/raw/refs/heads/main/LaunchAgents/com.user.homebrew-update.plist
 curl -LO https://github.com/JMR-dev/mac_util_scripts/raw/refs/heads/main/LaunchAgents/com.user.restart-browsers.plist
-
+curl -LO https://github.com/JMR-dev/mac_util_scripts/raw/refs/heads/main/LaunchAgents/update-nvim-config.plist
 # Install Commands
 launchctl bootstrap gui/"$(id -u)" com.user.homebrew-update.plist
 launchctl bootstrap gui/"$(id -u)" com.user.restart-browsers.plist
+launchctl bootstrap gui/"$(id -u)" com.user.update-nvim-config.plist
 
 echo 'Automation agents installed'
